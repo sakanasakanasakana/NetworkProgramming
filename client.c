b739c7a3b8b99ed0e0a5f4cece917bd26cc420e3
sakanasakanasakana <sakanamaxon@gmail.com>
2019-04-26 10:23:52 +0900

    20190426

---
+++ client.c
@@ -0,0 +1,33 @@
+#include <sys/socket.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+void commun(int sock){
+	char buf[256];
+	int len_r;
+	char*message = "(」・ω・)」うー！(／・ω・)／にゃー！";
+	send(sock,message,strlen(message),0);
+	len_r=recv(sock,buf,256,0);
+	buf[len_r] = '\0';
+	
+	printf("%s\n",buf);
+	return ;
+}
+
+int main(int argc, char** argv){
+	int sock = socket(PF_INET,SOCK_STREAM, 0);
+	
+	struct sockaddr_in target;
+	
+	target.sin_family = AF_INET;
+	target.sin_addr.s_addr = inet_addr("10.13.64.20");
+	target.sin_port = htons(10001);
+	
+	connect(sock,(struct sockaddr*)&target, sizeof(target));
+	
+	commun(sock);
+	
+	close(sock);
+	return 0;
+}

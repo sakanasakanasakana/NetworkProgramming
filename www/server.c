64f5f9beafd15902207aa38bfc10cab816f1e56c
sakanasakanasakan <sakanamaxon@gmail.com>
2019-07-19 10:34:06 +0900

    20190719

--- www/server.c
+++ www/server.c
@@ -4,8 +4,10 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <stdlib.h>
+#include <regex.h>
 
 #define BUF_SIZE 256
+#define result_SIZE 100
 //http://localhost/
 void DieWithError(char *errorMessage){
 	perror(errorMessage);
@@ -15,52 +17,82 @@ void commum (int sock){
 	char buf[BUF_SIZE];
 	char buf_old[BUF_SIZE];
 	char buf2[2*BUF_SIZE];
+	
+	regex_t regBuf;
+	regmatch_t regMatch[1];
 	int len_r; 
 	char response[BUF_SIZE];
+	char result[result_SIZE];
+	const char *pattern = "GET[^\\n] + HTTP";
 	
+	char *uri;
+	result[0] = '\n';
 	buf_old[0] = '\0';
-	while((len_r=recv(sock,buf,BUF_SIZE,0)) > 0){
+	
+	
+	if(regcomp(&regBuf, pattern, REG_EXTENDED | REG_NEWLINE)!=0){
+		DieWithError("regcomp failed");
+	}
+	
+	
+	while((len_r = recv(sock,buf,BUF_SIZE,0)) > 0){
 		buf[len_r] = '\0';
 		sprintf(buf2,"%s%s",buf_old,buf);
 		
-	if(strstr(buf,"\r\n\r\n")){
+		if(regexec(&regBuf, buf2, 1, regMatch, 0)!=0){
+		int startIndex = regMatch[0].rm_so;
+		int endIndex = regMatch[0].rm_eo;
+		strncpy(result, buf2 + startIndex, endIndex - startIndex);
+	}
+	if(strstr(buf2,"\r\n\r\n")){
 		break;
 	}
-	}
 		sprintf(buf_old, "%s",buf);
+	}
 	
-	if (len_r <= 0)
+	if (result[0]!='\n'){
+	uri = strtok(result," ");
+	uri = strtok(NULL," ");
+	printf("%s\n",uri);
+}else{
+	DieWithError("No URI");
+}
+	
+	regfree(&regBuf);
+	
+	if (len_r <= 0){
 		DieWithError("received() failed.");
-    
+	}
     printf("received HTTP Request.\n");
 
     sprintf(response, "HTTP/1.1 200 OK\r\n");
-    if(send(sock, response, strlen(response), 0) != strlen(response))
-        DieWithError("send() sent a message of unexpected bytes");
+	if(send(sock, response, strlen(response), 0) != strlen(response)){
+        DieWithError("send() sent a message of unexpected bytes");}
     
     sprintf(response, "Content-Type: text/html; charset=utf-8\r\n");
-    if(send(sock, response, strlen(response), 0) != strlen(response))
-        DieWithError("send() sent a message of unexpected bytes");
+	if(send(sock, response, strlen(response), 0) != strlen(response)){
+        DieWithError("send() sent a message of unexpected bytes");}
         
     sprintf(response, "\r\n");
-    if(send(sock, response, strlen(response), 0) != strlen(response))
-        DieWithError("send() sent a message of unexpected bytes");
+	if(send(sock, response, strlen(response), 0) != strlen(response)){
+        DieWithError("send() sent a message of unexpected bytes");}
     
     sprintf(response, "<!DOCTYPE html><html><head><title>");
-    if(send(sock, response, strlen(response), 0) != strlen(response))
-        DieWithError("send() sent a message of unexpected bytes");
+	if(send(sock, response, strlen(response), 0) != strlen(response)){
+        DieWithError("send() sent a message of unexpected bytes");}
     
-    sprintf(response, "ネットワークプログラミングのwebサイト");
-    if(send(sock, response, strlen(response), 0) != strlen(response))
-        DieWithError("send() sent a message of unexpected bytes");
+	sprintf(response, "ネットワークプログラミングのwebサイト");
+	if(send(sock, response, strlen(response), 0) != strlen(response)){
+        DieWithError("send() sent a message of unexpected bytes");}
     
     sprintf(response, "</title></head><body>ネットワークダイスキ</body></html>");
-		if(send(sock, response, strlen(response), 0) != strlen(response))
-        DieWithError("send() sent a message of unexpected bytes");
+	if(send(sock, response, strlen(response), 0) != strlen(response)){
+        DieWithError("send() sent a message of unexpected bytes");}
 
 
-	if(send(sock,buf,strlen(buf),0)!=strlen(buf))
-	DieWithError("send()sent a message of unexpected bytes");
+	if(send(sock,buf,strlen(buf),0)!=strlen(buf)){
+	DieWithError("send()sent a message of unexpected bytes");}
+
 }
 
 int main(int argc, char **argv){
